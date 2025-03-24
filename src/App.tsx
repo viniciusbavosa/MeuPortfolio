62c2a79937e57d091dc24d997cf8bfe6d98089d0
viniciusbavosa <viniciusbavosa@outlook.com>
2025-03-24 09:41:47 -0300

    refactor: switch Google Analytics to Versel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { routes } from "~/routes";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <RouterProvider router={routes} />
     </>
   );
