23d2040967199806b90b5511ee97d6a4c49e4b33
viniciusbavosa <viniciusbavosa@outlook.com>
2025-05-17 19:48:19 -0300

    refactor: better footer UI

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer>
-      <small>Vinícius Bavosa &copy; Todos os direitos reservados</small>
+      <small>
+        <span>Vinícius Bavosa</span> &copy; Todos os direitos reservados
+      </small>
     </footer>
   );
 }
