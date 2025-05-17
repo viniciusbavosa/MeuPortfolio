29cd0b6fc86f0b17c309c258478f4cb1d77c82ba
viniciusbavosa <viniciusbavosa@outlook.com>
2025-05-17 19:03:26 -0300

    feat: Add storybook

---
+++ src/components/footer/Footer.stories.tsx
@@ -0,0 +1,9 @@
+import Footer from "./footer";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/Footer",
+  component: Footer,
+} as Meta;
+
+export const Default: StoryObj = {};
---
+++ src/components/loader/Loader.stories.tsx
@@ -0,0 +1,9 @@
+import { Loader } from "./loader";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/Loader",
+  component: Loader,
+} as Meta;
+
+export const Default: StoryObj = {};
