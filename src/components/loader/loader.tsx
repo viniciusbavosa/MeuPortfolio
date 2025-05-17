caffad3cb45d065b9e1cd2bed53391a62ec9d1ce
viniciusbavosa <viniciusbavosa@outlook.com>
2025-05-17 19:03:10 -0300

    feat: add a loader animation

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,7 +1,11 @@
+import { FaSpinner } from "react-icons/fa";
+
 export function Loader() {
   return (
     <section className="section-content-wrapper">
-      <h1 className="section-title">Os artigos estão sendo carregados...</h1>
+      <h1 className="section-title">
+        <FaSpinner className="spinner-loader" />
+      </h1>
     </section>
   );
 }
